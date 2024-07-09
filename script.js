719de18d92a8febfba62b0baafee628fedbe09fb
LOGESHP01 <samlogesh04@gmail.com>
2024-07-09 16:01:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('off').addEventListener('click',function(){
+    document.getElementById("image").src="https://media.istockphoto.com/photos/light-bulb-in-the-dark-with-blue-hues-picture-id156642734?k=6&m=156642734&s=612x612&w=0&h=U_AYzTMVs-RV1lJRXElUlbNY87F1j6rG1up2hNDGjP0="
+})
+document.getElementById('on').addEventListener('click',function(){
+    document.getElementById("image").src="https://th.bing.com/th/id/OIP.3ssMTyfuh6c9njDZU6Z4bQHaE8?rs=1&pid=ImgDetMain"
+})
